329e25636b638f1a869705e8a005f0b0ed562514
Daylam Tayari <git@tayari.gg>
2026-02-12 23:13:19 -0600

    Added errcheck nolint to crypto/rand.Read

--- pkg/superproductivity/superproductivity.go
+++ pkg/superproductivity/superproductivity.go
@@ -23,7 +23,7 @@ func NewCompleteBackup(data AppDataComplete) *CompleteBackup {
 func generateID() string {
 	// Generate 16 random bytes
 	b := make([]byte, 16)
-	rand.Read(b)
+	rand.Read(b) //nolint:errcheck - An error can never be handled as if it errors crypto/rand panics
 
 	// Encode to base64 URL-safe format and trim to 21 characters
 	id := base64.URLEncoding.EncodeToString(b)
